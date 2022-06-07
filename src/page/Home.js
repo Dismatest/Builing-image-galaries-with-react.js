607aebaf9a4dd9f5695e4c5be5c7c649d2835752
Dismatest <billclintonogot88@gmail.com>
2022-06-07 13:10:39 +0300

    resolving dependancies

--- src/page/Home.js
+++ src/page/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
             <div className="flex justify-space-between mt-10">
                 <div className="w-1/4 mx-auto p-6 bg-white rounded-lg shadow-xl">
                     <img className="w-full" src="https://www.bing.com/th?id=AMMS_7cc2bb63a5caa1e1e5279a3dc06acc55&w=156&h=112&c=7&o=6&dpr=1.5&pid=SANGAM" alt="ogot clinton!" />
-                    <div className="cursor-pointe decoration-solidr decoration-solid">
+                    <div className="cursor-pointe decoration-solid decoration-solid">
                     <h3 className="text-center m-2">Detect Object</h3> 
                     <p className="text-center ">In this project we use tensorflow js model to detect objects</p>
                     </div>
