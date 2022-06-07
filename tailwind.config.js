68f4cf3e7e7eaf814cfc4a9a365349e274be1b9f
Dismatest <billclintonogot88@gmail.com>
2022-06-07 14:21:00 +0300

    fixed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
